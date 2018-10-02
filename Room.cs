ab843b142517490dbeb2451bccf3a78c4a7f2033
rekreider16 <43345757+rekreider16@users.noreply.github.com>
2018-10-02 09:07:58 -0500

    Add files via upload

---
+++ Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenHouse
+{
+    class Room
+    {
+        public string Name { get; set; }
+        public bool HasBeenChecked { get; set; }
+        public string Description { get; set; }
+
+        public Room()
+        {
+
+        }
+    }
+}
